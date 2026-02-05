72ee855868af928221d776b1e08cdc6245c3ca01
Reema Dhanwani <reemadhanwani1234@gmail.com>
2026-02-06 00:36:42 +0530

    Added Player model

---
+++ models/Players/ChessPlayer.java
@@ -0,0 +1,35 @@
+package models.Players;
+
+import models.Board.ChessBoard;
+import models.Pieces.ChessPiece;
+import models.Pieces.Piece;
+import models.Pieces.PieceName;
+
+import java.util.Map;
+
+public abstract class ChessPlayer extends Player {
+    private final Map<PieceName, ChessPiece> pieces;
+    private final ChessBoard board;
+
+    public ChessPlayer(String name,ChessBoard board,Map<PieceName, ChessPiece> pieces) {
+        super(name);
+        this.board = board;
+        this.pieces = pieces;
+    }
+
+    public Map<PieceName, ChessPiece> getPieces() {
+        return pieces;
+    }
+
+    public ChessBoard getBoard() {
+        return board;
+    }
+
+    public ChessPiece getPiece(PieceName pieceName) {
+        if(!pieces.containsKey(pieceName)) {
+            throw new IllegalArgumentException("Piece not found");
+        }
+
+        return pieces.get(pieceName);
+    }
+}
---
+++ models/Players/HumanChessPlayer.java
@@ -0,0 +1,49 @@
+package models.Players;
+
+import models.Board.ChessBoard;
+import models.Pieces.ChessPiece;
+import models.Pieces.Move;
+import models.Pieces.PieceName;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class HumanChessPlayer extends ChessPlayer {
+    public HumanChessPlayer(String name, ChessBoard board, Map<PieceName, ChessPiece> pieces) {
+        super(name,board,pieces);
+    }
+
+    @Override
+    public Move makeMove() {
+        // 1. Display the current state of the board
+
+        this.getBoard().display();
+
+        // 2. prepare the scanner for taking input
+        Scanner sc = new Scanner(System.in);
+
+        // 3. Take the input of which cell user wants to select and then move piece from that cell
+        System.out.println("Please enter the coordinates of the piece you would like to move:");
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        // 4. Check if there is any piece present or not ?
+        if(!this.getBoard().getBoard().get(row).get(col-'a').hasPiece()) {
+            throw new RuntimeException("no Piece found at this location");
+        }
+
+        // 5. Take input for destination
+        System.out.println("Please enter the coordinates where you would like to move your piece");
+        int desRow = sc.nextInt();
+        int desCol = sc.nextInt();
+
+        // 6. check if we can move or not ?
+
+        ChessPiece pieceToMove = this.getBoard().getBoard().get(row).get(col-'a').getChessPiece().get();
+        if(!pieceToMove.canMove(this.getBoard().getBoard().get(row).get(col-'a'),this.getBoard().getBoard().get(desRow).get(desCol-'a'),this.getBoard())) {
+            throw new RuntimeException("piece can not be moved");
+        }
+        return new Move(this.getBoard().getBoard().get(row).get(col-'a'),this.getBoard().getBoard().get(desRow).get(desCol-'a'));
+
+    }
+}
